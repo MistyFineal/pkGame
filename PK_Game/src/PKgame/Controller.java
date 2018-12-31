eaab2e7dc7722df2e098d7f541085d0b2d68f925
MistyFineal <soulbrew40@gmail.com>
2019-01-01 02:58:26 +0900

    01.01-02:57 ゴールの描画処理追加

--- PK_Game/src/PKgame/Controller.java
+++ PK_Game/src/PKgame/Controller.java
@@ -6,17 +6,25 @@ import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.*;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 
+import java.awt.*;
 import java.net.Socket;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class Controller implements Initializable {
+    @FXML private Label titleText;
     @FXML private Button ok_Button;
     @FXML private Button decision_Button;
     @FXML private TextField text_Field;
     @FXML private ListView listView;
     @FXML private PK_ClientRunnable pkClient;
+    @FXML private ImageView goalView;
 
     private ObservableList<String> items;
 
@@ -24,6 +32,8 @@ public class Controller implements Initializable {
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         pkClient = null;
+        titleText.setVisible(true);
+        goalView.setVisible(false);
         items = FXCollections.observableArrayList();
         listView.setItems(items);
         listView.getItems().add("接続するサーバのIPアドレスを入力し, ゲーム開始ボタンを押してください.");
@@ -41,6 +51,8 @@ public class Controller implements Initializable {
                 pkClient = new PK_ClientRunnable(socket, this);
                 ok_Button.setVisible(false);
                 decision_Button.setVisible(true);
+                titleText.setVisible(false);
+                goalView.setVisible(true);
             } catch (Exception e) {
                 sendMessageToGUI("接続に失敗しました.");
                 e.printStackTrace();
@@ -56,5 +68,6 @@ public class Controller implements Initializable {
         listView.getItems().add(msg);
         listView.scrollTo(listView.getItems().size()-1);
     }
+
 
 }
